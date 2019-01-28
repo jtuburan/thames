dc6cc2cc4911716fb517992a5d0419d62f89f88b
jason tuburan <jasontuburan@gmail.com>
2019-01-29 01:18:11 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="index.css"/>
+</head>
+<body>
+    <div  style="background-color:black;">
+        <div class="header">
+            <div class="container">
+                <div class="nav-bar">
+                    <h1>nav-bar</h1>
+                </div>
+            </div>
+        </div>
+        <div class="main-body" style="background-color:blue;">
+            <div class="container" style="background-color:white;">
+                <div class="content row">
+
+                    <div class="col-md-3 card-block">
+                        <article class="course">
+                            <a href="" class="a-link">
+                                <div class="card">
+                                    <header class="course-image">
+                                        <div class="cover-image">
+                                            <span  style="backgound-image: url('')">
+                                                <!-- image src goes in span tag -->
+                                            </span>
+                                        </div>
+                                    </header>
+                                    <div class="course-info">
+                                        <h2 class="course-name">
+                                            <!-- course code -->
+                                            <span class="course-code">CS101</span>
+                                            <!-- course title -->
+                                            <span class="course-title">Introduction to computer science</span>
+                                        </h2>
+                                    </div> 
+                                        <!-- course date -->
+                                    <div class="course-date">
+                                        Starts Jan 1, 2018
+                                    </div>
+                                </div>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer" style="background-color:yellow">
+                <div class="footer-content">
+                    <h1>Footer</h1>
+                </div>
+        </div>
+        
+
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+</html>
---
+++ inside.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="index.css"/>
+</head>
+<body>
+    <div  style="background-color:black;">
+        <div class="header">
+            <div class="container">
+                <div class="nav-bar">
+                    <h1>nav-bar</h1>
+                </div>
+            </div>
+        </div>
+        <div class="main-body">
+            <div class="course-intro" >
+                <div class="container">
+                    <h1 class="title-intro">
+                        <!-- title code -->
+                        <span class="title-code">BUS249</span>
+                        <br/>
+                        <!-- title -->
+                        <span class="title">Human Resource Management</span>
+                        <br/>
+                        <!-- title definition -->
+                        <span class="title-definition">
+                            Understand the theory and practice of managing human resources, especially in the Philippine bussiness context.
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div class="duration-row">
+                <div class="container">
+                    <div class="row">
+                    <!-- start date -->
+                    <div class="col-md-4">
+                        <span class="duration-start">START DATE</span>
+                        <h5>November 19, 2019</h5>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="duration-start">DURATION</span>
+                        <h5>14 WEEKS</h5>
+                    </div>
+                    <!-- enrollment button -->
+                    <div class="col-md-4">
+                        <button class="enrollment-button">Enrollment is Closed</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container" style="background-color:white;">
+                <div>
+                        asdas
+                </div>
+            </div>
+        </div>
+        <div class="footer" style="background-color:yellow">
+                <div class="footer-content">
+                    <h1>Footer</h1>
+                </div>
+        </div>
+        
+
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+</html>
